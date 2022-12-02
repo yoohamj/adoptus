18ef501aabbe4c9842b4e5598a939aba18023e23
Yooham Jung <yoohamjung@Yoohamui-MacBookAir.local>
2022-12-02 16:27:27 -0500

    fix

--- components/banner.js
+++ components/banner.js
@@ -5,6 +5,7 @@ function Banner() {
         <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
             <Image
                 src="https://links.papareact.com/0fm"
+                alt = "banner background"
                 layout="fill"
                 objectFit="cover"
             />
@@ -13,7 +14,7 @@ function Banner() {
                     Not sure where to go? Perfect.
                 </p>
                 <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
-                    I'm flexible
+                    Search Nearby
                 </button>
             </div>
         </div>
