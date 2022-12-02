0d7bbc5f53549ae915caa4f17ec412bcf208d2e6
Yooham Jung <yoohamjung@Yoohamui-MacBookAir.local>
2022-12-01 22:59:38 -0500

    initial commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["links.papareact.com", "www.jsonkeeper.com", "jsonkeeper.com."],
+          },
+  env: {
+    mapbox_key: "pk.eyJ1IjoicGljb3p6aW1pY2hlbGUiLCJhIjoiY2t0YjZtZmpuMXQ0dDJ2bjloM3ZxM2R2dyJ9.vpLuQlobE_kubcyl02VIdg"
+      }
 }
 
 module.exports = nextConfig
