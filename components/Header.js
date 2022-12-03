ce61fd65b0a42aeddc64dbe3020041d7592172cb
Yooham Jung <yoohamjung@Yoohamui-MacBookAir.local>
2022-12-03 14:50:12 -0500

    buttondrop

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,15 @@
 import Image from "next/image";
 import Logo from "../images/logo.svg"
+
 import { GlobeAltIcon, Bars3Icon, UserCircleIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import { Menu, Transition } from '@headlessui/react'
+
 import { useRouter } from "next/dist/client/router";
-import { useState } from "react";
+import { useState, Fragment } from "react";
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
 
 function Header() {
     const [searchInput, setSearchInput] = useState("");
@@ -47,8 +54,80 @@ function Header() {
                 <p className="hidden md:inline cursor-pointer">Become a host</p>
                 <GlobeAltIcon className="h-6 cursor-pointer" />
                 <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
-                    <Bars3Icon className="h-6" />
-                    <UserCircleIcon className="h-6" />
+                    <Menu as="div" className="relative inline-block text-left">
+                        <Menu.Button className="inline-block ">
+                            <Bars3Icon className="h-6 inline-block" />
+                            <UserCircleIcon className="h-6 inline-block" />
+                        </Menu.Button>
+                        <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-100"
+                            enterFrom="transform opacity-0 scale-95"
+                            enterTo="transform opacity-100 scale-100"
+                            leave="transition ease-in duration-75"
+                            leaveFrom="transform opacity-100 scale-100"
+                            leaveTo="transform opacity-0 scale-95"
+                        >
+                            <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                <div className="py-1">
+                                    <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                        href="#"
+                                        className={classNames(
+                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                            'block px-4 py-2 text-sm'
+                                        )}
+                                        >
+                                        Sign Up
+                                        </a>
+                                    )}
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                        href="#"
+                                        className={classNames(
+                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                            'block px-4 py-2 text-sm'
+                                        )}
+                                        >
+                                        Log In
+                                        </a>
+                                    )}
+                                    </Menu.Item>
+                                </div>
+                                <div className="py-1">
+                                    <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                        href="#"
+                                        className={classNames(
+                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                            'block px-4 py-2 text-sm'
+                                        )}
+                                        >
+                                        Archive
+                                        </a>
+                                    )}
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                        href="#"
+                                        className={classNames(
+                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                            'block px-4 py-2 text-sm'
+                                        )}
+                                        >
+                                        Move
+                                        </a>
+                                    )}
+                                    </Menu.Item>
+                                </div>
+                                </Menu.Items>
+                            </Transition>
+                         </Menu>
                 </div>
             </div>
 
