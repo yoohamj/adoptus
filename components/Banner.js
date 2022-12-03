5d28fbf74076bd3c6b6a72bb0bd9d748d5bfbd3f
Yooham Jung <yoohamjung@Yoohamui-MacBookAir.local>
2022-12-02 20:11:35 -0500

    home button

--- components/Banner.js
+++ components/Banner.js
@@ -1,7 +1,14 @@
 import Image from "next/image";
+import { useRouter } from "next/dist/client/router"
 
 function Banner() {
+    const router = useRouter();
+    const search = () => {
+        router.push('/search');
+    }; 
+
     return (
+        
         <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
             <Image
                 src="https://links.papareact.com/0fm"
@@ -11,9 +18,9 @@ function Banner() {
             />
             <div className="absolute top-1/2 w-full text-center">
                 <p className="text-sm sm:text-lg">
-                    Not sure where to go? Perfect.
+                    Find your fur friend forever.
                 </p>
-                <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
+                <button onClick={search} className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
                     Search Nearby
                 </button>
             </div>
---
+++ pages/search.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+function Search() {
+  return (
+    <div>
+        <Header />
+
+        <main className='flex-grow pt-14 px-6'>
+            <section>
+                <p className='text-xs'> 300+ Stays for 5 number of guests</p>
+
+                <h1 className='text-3xl font-semibold mt-2 mb-6'>Pets in Toronto</h1>
+
+                <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
+                    <p className='button'>Age</p>
+                    <p className='button'>Gender</p>
+                    <p className='button'>Fee</p>
+                    <p className='button'>Neuteured</p>
+                    <p className='button'>More filters</p>
+                </div>
+
+            </section>
+
+        </main>
+
+        <Footer />
+    </div>
+  )
+}
+
+export default Search
