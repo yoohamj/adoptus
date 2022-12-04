2d479d9f2eeb33ee9d88647e32a65370922cee2e
Yooham Jung <yoohamjung@Yoohamui-MacBookAir.local>
2022-12-04 01:16:06 -0500

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import Footer from '../components/Footer'
 import React, { useEffect, useState } from 'react';
 import { Amplify, Auth, Hub } from 'aws-amplify';
 import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
-import awsconfig from '../aws-exports'
+import awsconfig from './aws-exports' 
 
 Amplify.configure(awsconfig);
 
