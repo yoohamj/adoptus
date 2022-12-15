317ae14d4a3541f18909de0cbf19b0b72f494580
Yooham Jung <yoohamjung@Yoohamui-MacBookAir.local>
2022-12-14 23:54:32 -0500

    team

--- components/Header.js
+++ components/Header.js
@@ -113,7 +113,7 @@ function Header() {
 
             {/* Right */}
             <div className="flex items-center space-x-4 justify-end text-gray-500">
-                <p className="hidden md:inline cursor-pointer">Become a host</p>
+                <p className="hidden md:inline cursor-pointer">Register a pet</p>
                 <GlobeAltIcon className="h-6 cursor-pointer" />
                 <div className="flex items-center space-x-2 border-2 p-2 rounded-full hover:shadow-xl active:scale-90 transition duration-150">
                     {
